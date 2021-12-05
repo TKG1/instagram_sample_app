99f60f82e9d421c0a0471dd36027df8782eb85e9
TKG1 <kouheideal@gmail.com>
2021-12-05 12:00:26 +0900

    Modify: お気に入り機能をajax化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,13 @@
 class FavoritesController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
     favorite = current_user.favorites.build(post_id: params[:post_id])
     favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     favorite = current_user.favorites.find(params[:id])
-    favorite.destroy
-    redirect_back(fallback_location: root_path)
+    favorite.destroy!
   end
 end
